6b9ec4030bc20d33fd7a37c1aabd8dd164dcedab
Lorenz <l.lione@remoteitalia.com>
2025-02-24 09:53:35 +0100

    Base

---
+++ src/main/java/bw5/energyservices/EnergyservicesApplication.java
@@ -0,0 +1,13 @@
+package bw5.energyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bw5/energyservices/EnergyservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package bw5.energyservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
