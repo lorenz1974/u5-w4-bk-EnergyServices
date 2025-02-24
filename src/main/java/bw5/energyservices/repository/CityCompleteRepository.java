a3f80cdb8af267a50111ac1346a83b83ba6bd36a
Lorenz <l.lione@remoteitalia.com>
2025-02-24 15:28:04 +0100

    Corretto CityCompleteRepository

--- src/main/java/bw5/energyservices/repository/CityCompleteRepository.java
+++ src/main/java/bw5/energyservices/repository/CityCompleteRepository.java
@@ -1,9 +1,9 @@
+package bw5.energyservices.repository;
+
 import bw5.energyservices.model.CityComplete;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-package bw5.energyservices.repository;
 
 @Repository
 public interface CityCompleteRepository extends JpaRepository<CityComplete, Long> {
