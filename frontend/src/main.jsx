c850d5f2dfccc5f942743ab9e4912095f2959677
SERENA FERRARO <serenaferraro21@gmail.com>
2025-02-25 11:45:28 +0000

    componente login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Login from "./components/Login.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    {" "}
+    <Login />
+  </div>
+);
